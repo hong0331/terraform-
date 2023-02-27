5b04eacf0a0e17d41006d93f2f937cc4e4117dff
hong0331 <0331912@gmail.com>
2023-02-28 00:24:02 +0900

    deploying the instances w/ TF

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+key_name        = "zoey_key" #your key name 
+public_key_path = "/home/ubuntu/.ssh/zoey_key.pub" #your public key path
--- variable.tf
+++ variable.tf
@@ -4,6 +4,29 @@ variable "vpc_cidr" {
 }
 
 variable "access_ip" {
-  type    = list(string)
-  default = ["0.0.0.0/0"] # allow all the traffic
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "main_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "main_vol_size" {
+  type    = number
+  default = 8
+}
+
+variable "main_instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
 }
